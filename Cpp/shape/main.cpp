64748707eefb48946da642d9e70bb274f4392d91
serkanaydin84 <serkanaydintech@gmail.com>
2024-09-25 22:42:41 +0300

    Soyut Sınıflar konusu bitirildi

--- Cpp/shape/main.cpp
+++ Cpp/shape/main.cpp
@@ -30,21 +30,44 @@ public:
 
 class Triangle : public Shape {
 public:
+    Triangle(int x, int y) : Shape(x, y) {
+        this->x = x;
+        this->y = y;
+    }
     void draw() override {
-        cout << "The triangle has been drawn. " << endl;
+        cout << "The triangle (" << x << "-" << y << ") has been drawn. " << endl;
     }
 };
 
 class Rectangle : public Shape {
 public:
+    Rectangle(int x, int y) : Shape(x, y) {
+        this->x = x;
+        this->y = y;
+    }
     void draw() override {
-        cout << "The rectangle has been drawn. " << endl;
+        cout << "The rectangle (" << x << "-" << y << ") has been drawn. " << endl;
+    }
+};
+
+class Square : public Rectangle {
+public:
+    Square(int x) : Rectangle(x, y) {
+        this->x = x;
+    }
+    void draw() override {
+        cout << "The square (" << x << ") has been drawn. " << endl;
     }
 };
 
 int main() {
-    Triangle item1 = Triangle();
+    Triangle item1 = Triangle(3, 5);
+    Rectangle item2 = Rectangle(6, 8);
+    Square item3 = Square(7);
+
     item1.draw();
+    item2.draw();
+    item3.draw();
 
     char c = getchar();
     return 0;
